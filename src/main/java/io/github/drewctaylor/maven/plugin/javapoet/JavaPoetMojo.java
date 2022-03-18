11f6f46c1f32f8816d4578044d5823d33852ec3e
Drew Taylor <3899848+drewctaylor@users.noreply.github.com>
2022-03-18 18:54:27 -0400

    Fixes #13: Support Java 8; support deployment to central; remove dependency on functional java.

--- src/main/java/io/github/drewctaylor/maven/plugin/javapoet/JavaPoetMojo.java
+++ src/main/java/io/github/drewctaylor/maven/plugin/javapoet/JavaPoetMojo.java
@@ -1,36 +1,33 @@
 package io.github.drewctaylor.maven.plugin.javapoet;
 
 import com.squareup.javapoet.JavaFile;
-import fj.F2;
-import fj.F3;
-import fj.P2;
-import fj.Try;
-import fj.TryEffect;
-import fj.Unit;
-import fj.data.HashMap;
-import fj.data.List;
-import fj.data.NonEmptyList;
-import fj.data.Option;
-import fj.data.Validation;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
-import static fj.P.p;
-import static fj.Semigroup.nonEmptyListSemigroup;
-import static fj.data.HashMap.fromMap;
-import static fj.data.List.iterableList;
-import static fj.data.NonEmptyList.nel;
-import static fj.data.Validation.sequence;
 import static java.lang.String.format;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static java.util.Objects.requireNonNull;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Stream.concat;
+import static java.util.stream.Stream.empty;
+import static java.util.stream.Stream.of;
+import static java.util.stream.StreamSupport.stream;
 
 /**
  * Invoke the given methods with the given string parameters; write the returned JavaFile Stream to the given directory.
@@ -38,94 +35,145 @@ import static java.util.Objects.requireNonNull;
 @Mojo(name = "generate-sources", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public final class JavaPoetMojo extends AbstractMojo
 {
-    private static Validation<NonEmptyList<Exception>, File> directoryFor(
+    private static <T> BiFunction<Function<List<Exception>, T>, BiFunction<String, String, T>, T> classNameMethodNameFor(
+            final Matcher matcher,
+            final String classNameMethodName)
+    {
+        final int groupForClassName = 1;
+        final int groupForMethodName = 4;
+
+        return matcher.matches() ?
+                (
+                        fExceptionList,
+                        fClassNameMethodName) -> fClassNameMethodName.apply(matcher.group(groupForClassName), matcher.group(groupForMethodName)) :
+                (
+                        fExceptionList,
+                        fClassNameMethodName) -> fExceptionList.apply(singletonList(new Exception(format("The plugin could not parse the method '%s'.", classNameMethodName))));
+    }
+
+    private static <T> BiFunction<Function<List<Exception>, T>, Function<List<JavaFile>, T>, T> javaFileList(
+            final String className,
+            final String methodName,
+            final String parameter)
+    {
+        try
+        {
+            final List<JavaFile> javaFileList = stream(((Iterable<JavaFile>) Class.forName(className).getMethod(methodName, String.class).invoke(null, parameter)).spliterator(), false).collect(toList());
+            return (
+                    fExceptionList,
+                    fJavaFileIterable) -> fJavaFileIterable.apply(javaFileList);
+        }
+        catch (final ClassNotFoundException | IllegalAccessException | InvocationTargetException | NoSuchMethodException exception)
+        {
+            return (
+                    fExceptionList,
+                    fJavaFileList) -> fExceptionList.apply(singletonList(exception));
+        }
+    }
+
+    private static <T> BiFunction<Function<List<Exception>, T>, Function<File, T>, T> directoryFor(
             final String path)
     {
-        return Try.f(() ->
-        {
-            final File file = Paths.get(path).toFile();
+        final File file = Paths.get(path).toFile();
 
-            if (file.isFile())
-            {
-                throw new Exception(format("The path exists and references a file '%s'.", path));
-            }
-
-            if (!file.exists() && !file.mkdirs())
-            {
-                throw new Exception(format("The plugin could not create the directory '%s'.", path));
-            }
-
-            return file;
-        })._1().f().map(NonEmptyList::nel);
+        return file.isFile() ?
+                (
+                        fExceptionList,
+                        fFile) -> fExceptionList.apply(singletonList(new Exception(format("The path exists and references a file '%s'.", path)))) :
+                !file.exists() && !file.mkdirs() ?
+                        (
+                                fExceptionList,
+                                fFile) -> fExceptionList.apply(singletonList(new Exception(format("The plugin could not create the directory '%s'.", path)))) :
+                        (
+                                fExceptionList,
+                                fFile) -> fFile.apply(file);
     }
 
-    private static Validation<NonEmptyList<Exception>, List<JavaFile>> javaFileStreamListFor(
-            final HashMap<String, String> classNameMethodNameParameterMap)
+    private static <T> BiFunction<Function<List<Exception>, T>, Function<List<JavaFile>, T>, T> javaFileStreamListFor(
+            final Map<String, String> classNameMethodNameParameterMap)
     {
-        final var regexForIdentifier = "[\\p{Alpha}_$][\\p{Alpha}\\p{Digit}_$]*";
-        final var regex = format("((%s)(\\.%s)*)\\.(%s)", regexForIdentifier, regexForIdentifier, regexForIdentifier);
-        final var pattern = Pattern.compile(regex);
-        final var groupForClassName = 1;
-        final var groupForMethodName = 4;
+        final String regexForIdentifier = "[\\p{Alpha}_$][\\p{Alpha}\\p{Digit}_$]*";
+        final String regex = format("((%s)(\\.%s)*)\\.(%s)", regexForIdentifier, regexForIdentifier, regexForIdentifier);
+        final Pattern pattern = Pattern.compile(regex);
 
-        final F2<Matcher, String, Validation<NonEmptyList<Exception>, P2<String, String>>> classNameMethodNameFor = (
-                matcher,
-                classNameMethodName) -> Option
-                        .iif(matcher.matches(), () -> p(matcher.group(groupForClassName), matcher.group(groupForMethodName)))
-                        .toValidation(nel(new Exception(format("The plugin could not parse the method '%s'.", classNameMethodName))));
-
-        @SuppressWarnings("unchecked")
-        final F3<String, String, String, Validation<NonEmptyList<Exception>, List<JavaFile>>> javaFileListFor = (
-                className,
-                methodName,
-                parameter) -> Try.f(() -> Class.forName(className).getMethod(methodName, String.class).invoke(null, parameter))._1()
-                        .map(object -> iterableList((Iterable<JavaFile>) object))
-                        .f().map(NonEmptyList::nel);
-
-        return sequence(nonEmptyListSemigroup(), classNameMethodNameParameterMap.toList()
-                .map(p -> classNameMethodNameFor.f(pattern.matcher(p._1()), p._1()).map(pInner -> pInner.append(p._2())))
-                .map(validation -> validation.bind(p -> javaFileListFor.f(p._1(), p._2(), p._3()))))
-                        .map(List::join);
+        return classNameMethodNameParameterMap.keySet().stream()
+                .map(classNameMethodName -> JavaPoetMojo.<BiFunction<Function<List<Exception>, T>, Function<List<JavaFile>, T>, T>>classNameMethodNameFor(pattern.matcher(classNameMethodName), classNameMethodName).apply(
+                        exceptionList -> (
+                                fExceptionList,
+                                fJavaFileList) -> fExceptionList.apply(exceptionList),
+                        (
+                                className,
+                                methodName) -> JavaPoetMojo.<BiFunction<Function<List<Exception>, T>, Function<List<JavaFile>, T>, T>>javaFileList(className, methodName, classNameMethodNameParameterMap.get(classNameMethodName)).apply(
+                                        exceptionList -> (
+                                                fExceptionList,
+                                                fJavaFileList) -> fExceptionList.apply(exceptionList),
+                                        javaFileList -> (
+                                                fExceptionList,
+                                                fJavaFileList) -> fJavaFileList.apply(javaFileList))))
+                .reduce(
+                        (
+                                fExceptionList,
+                                fJavaFileList) -> fJavaFileList.apply(emptyList()),
+                        (
+                                validation1,
+                                validation2) -> (
+                                        fExceptionList,
+                                        fJavaFileList) -> validation1.apply(
+                                                exceptionList -> validation2.apply(
+                                                        exceptionListInner -> fExceptionList.apply(concat(exceptionList.stream(), exceptionListInner.stream()).collect(toList())),
+                                                        javaFileListInner -> fExceptionList.apply(exceptionList)),
+                                                javaFileList -> validation2.apply(
+                                                        exceptionListInner -> fExceptionList.apply(exceptionListInner),
+                                                        javaFileListInner -> fJavaFileList.apply(concat(javaFileList.stream(), javaFileListInner.stream()).collect(toList())))));
     }
 
-    static Validation<NonEmptyList<Exception>, List<Unit>> executeHelper(
+    private static Stream<Exception> writeTo(
+            final File file,
+            final JavaFile javaFile)
+    {
+        try
+        {
+            javaFile.writeTo(file);
+            return empty();
+        }
+        catch (final IOException ioException)
+        {
+            return of(ioException);
+        }
+    }
+
+    static List<Exception> executeHelper(
             final String path,
             final Map<String, String> classNameMethodNameParameterMap)
     {
         requireNonNull(path);
         requireNonNull(classNameMethodNameParameterMap);
 
-        final F2<File, JavaFile, Validation<NonEmptyList<Exception>, Unit>> writeTo = (
-                file,
-                javaFile) -> TryEffect.f(() -> javaFile.writeTo(file))._1()
-                        .f().map(NonEmptyList::nel);
-
-        return directoryFor(path).bind(file -> javaFileStreamListFor(fromMap(classNameMethodNameParameterMap)).bind(list -> sequence(nonEmptyListSemigroup(), list.map(javaFile -> writeTo.f(file, javaFile)))));
+        return JavaPoetMojo.<List<Exception>>directoryFor(path).apply(
+                identity(),
+                directory -> JavaPoetMojo.<List<Exception>>javaFileStreamListFor(classNameMethodNameParameterMap).apply(
+                        identity(),
+                        javaFileList -> javaFileList.stream().flatMap(javaFile -> writeTo(directory, javaFile)).collect(toList())));
     }
 
     /**
-     * A path to which to write the java file.
+     * A path to which to write a java file.
      */
-    @SuppressWarnings(
-    { "unused", "InstanceVariableMayNotBeInitialized" })
     @Parameter(alias = "path", defaultValue = "${project.basedir}/src/main/java/")
     private String path;
 
     /**
      * A map from a fully-qualified method name to a string parameter.
      */
-    @SuppressWarnings(
-    { "unused", "InstanceVariableMayNotBeInitialized" })
     @Parameter(alias = "methods", required = true)
     private Map<String, String> methods;
 
     /**
-     * Invoke the given methods with the given string parameters; write the returned JavaFile Stream to the given directory.
+     * Invoke a method with a string parameter; write a returned JavaFile Stream to the given directory.
      */
     @Override
     public void execute()
     {
-        executeHelper(path, methods)
-                .f().forEach(nel -> nel.forEach(exception -> getLog().error(exception)));
+        executeHelper(path, methods).forEach(exception -> getLog().error(exception));
     }
 }
--- src/test/java/io/github/drewctaylor/maven/plugin/javapoet/JavaPoetMojoTest.java
+++ src/test/java/io/github/drewctaylor/maven/plugin/javapoet/JavaPoetMojoTest.java
@@ -2,22 +2,19 @@ package io.github.drewctaylor.maven.plugin.javapoet;
 
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.TypeSpec;
-import fj.Unit;
-import fj.data.List;
-import fj.data.NonEmptyList;
-import fj.data.Validation;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.HashMap;
 
 import javax.lang.model.element.Modifier;
 
-import static fj.P.p;
-import static fj.data.HashMap.arrayHashMap;
 import static io.github.drewctaylor.maven.plugin.javapoet.JavaPoetMojo.executeHelper;
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.singletonMap;
+import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -30,15 +27,15 @@ public final class JavaPoetMojoTest
         {
         }
 
-        public static java.util.List<JavaFile> javaFileListWithoutParameter()
+        public static Iterable<JavaFile> javaFileListWithoutParameter()
         {
             return Collections.emptyList();
         }
 
-        public static java.util.List<JavaFile> javaFileListWithParameter(
+        public static Iterable<JavaFile> javaFileListWithParameter(
                 final String name)
         {
-            return Collections.singletonList(JavaFile.builder(
+            return singletonList(JavaFile.builder(
                     "io.github.drewctaylor.maven.plugin.javapoet",
                     TypeSpec.classBuilder(
                             name)
@@ -52,23 +49,23 @@ public final class JavaPoetMojoTest
     void testThrowsNullPointerException()
     {
         assertThrows(NullPointerException.class, () -> executeHelper("", null));
-        assertThrows(NullPointerException.class, () -> executeHelper(null, new HashMap<>(0)));
+        assertThrows(NullPointerException.class, () -> executeHelper(null, emptyMap()));
     }
 
     @Test
     void testPathMustBeValid()
     {
         assertEquals(
-                Validation.<Class<?>, List<Unit>>fail(Exception.class),
-                executeHelper("", new HashMap<>(0)).f().map(NonEmptyList::head).f().map(Object::getClass));
+                singletonList(Exception.class),
+                executeHelper("", emptyMap()).stream().map(Object::getClass).collect(toList()));
     }
 
     @Test
     void testPathMustBeDirectory()
     {
         assertEquals(
-                Validation.<Class<?>, List<Unit>>fail(Exception.class),
-                executeHelper(getClass().getResource("JavaPoetMojoTest.class").getFile(), new HashMap<>(0)).f().map(NonEmptyList::head).f().map(Object::getClass));
+                singletonList(Exception.class),
+                executeHelper(getClass().getResource("JavaPoetMojoTest.class").getFile(), emptyMap()).stream().map(Object::getClass).collect(toList()));
     }
 
     @Test
@@ -77,30 +74,30 @@ public final class JavaPoetMojoTest
         final String pathForTestClasses = getClass().getResource("./../../../").getPath();
 
         assertEquals(
-                Validation.<Class<?>, List<Unit>>fail(Exception.class),
-                executeHelper(pathForTestClasses, arrayHashMap(p("", "")).toMap()).f().map(NonEmptyList::head).f().map(Object::getClass));
+                singletonList(Exception.class),
+                executeHelper(pathForTestClasses, singletonMap("", "")).stream().map(Object::getClass).collect(toList()));
 
         assertEquals(
-                Validation.<Class<?>, List<Unit>>fail(Exception.class),
-                executeHelper(pathForTestClasses, arrayHashMap(p("ClassName", "")).toMap()).f().map(NonEmptyList::head).f().map(Object::getClass));
+                singletonList(Exception.class),
+                executeHelper(pathForTestClasses, singletonMap("ClassName", "")).stream().map(Object::getClass).collect(toList()));
 
         assertEquals(
-                Validation.<Class<?>, List<Unit>>fail(ClassNotFoundException.class),
-                executeHelper(pathForTestClasses, arrayHashMap(p("ClassName.methodName", "")).toMap()).f().map(NonEmptyList::head).f().map(Object::getClass));
+                singletonList(ClassNotFoundException.class),
+                executeHelper(pathForTestClasses, singletonMap("ClassName.methodName", "")).stream().map(Object::getClass).collect(toList()));
 
         assertEquals(
-                Validation.<Class<?>, List<Unit>>fail(NoSuchMethodException.class),
-                executeHelper(pathForTestClasses, arrayHashMap(p("io.github.drewctaylor.maven.plugin.javapoet.JavaPoetMojoTest$JavaFileFactory.methodName", "")).toMap()).f().map(NonEmptyList::head).f().map(Object::getClass));
+                singletonList(NoSuchMethodException.class),
+                executeHelper(pathForTestClasses, singletonMap("io.github.drewctaylor.maven.plugin.javapoet.JavaPoetMojoTest$JavaFileFactory.methodName", "")).stream().map(Object::getClass).collect(toList()));
 
         assertEquals(
-                Validation.<Class<?>, List<Unit>>fail(NoSuchMethodException.class),
-                executeHelper(pathForTestClasses, arrayHashMap(p("io.github.drewctaylor.maven.plugin.javapoet.JavaPoetMojoTest$JavaFileFactory.javaFileListWithoutParameter", "")).toMap()).f().map(NonEmptyList::head).f().map(Object::getClass));
+                singletonList(NoSuchMethodException.class),
+                executeHelper(pathForTestClasses, singletonMap("io.github.drewctaylor.maven.plugin.javapoet.JavaPoetMojoTest$JavaFileFactory.javaFileListWithoutParameter", "")).stream().map(Object::getClass).collect(toList()));
 
         assertEquals(
-                Validation.<Class<?>, List<Unit>>fail(InvocationTargetException.class),
-                executeHelper(pathForTestClasses, arrayHashMap(p("io.github.drewctaylor.maven.plugin.javapoet.JavaPoetMojoTest$JavaFileFactory.javaFileListWithParameter", "")).toMap()).f().map(NonEmptyList::head).f().map(Object::getClass));
+                singletonList(InvocationTargetException.class),
+                executeHelper(pathForTestClasses, singletonMap("io.github.drewctaylor.maven.plugin.javapoet.JavaPoetMojoTest$JavaFileFactory.javaFileListWithParameter", "")).stream().map(Object::getClass).collect(toList()));
 
-        executeHelper(pathForTestClasses, arrayHashMap(p("io.github.drewctaylor.maven.plugin.javapoet.JavaPoetMojoTest$JavaFileFactory.javaFileListWithParameter", "TestClass")).toMap());
+        executeHelper(pathForTestClasses, singletonMap("io.github.drewctaylor.maven.plugin.javapoet.JavaPoetMojoTest$JavaFileFactory.javaFileListWithParameter", "TestClass"));
 
         assertTrue(Paths.get(pathForTestClasses, "io", "github", "drewctaylor", "maven", "plugin", "javapoet", "TestClass.java").toFile().isFile());
     }
